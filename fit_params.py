3fb80ea307c125ff0e97802f830e001d2ce74252
Jason Leung <leung@astro.utoronto.ca>
2019-10-02 20:17:51 -0400

    Create classes in subroutines
    No more global variables as everything is consolidated into 'initvals' and
    'heater' objects. All functions are moved into methods of the 'heater' object.

--- fit_params.py
+++ fit_params.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os, sys, json
 import pygetdata as gd
 import numpy as np
@@ -6,19 +8,21 @@ from scipy import integrate
 from scipy.integrate import solve_ivp
 from scipy.optimize import least_squares
 from collections import OrderedDict
+from subroutines import initvals, heater
 
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["font.size"] = 14
 plt.rcParams["figure.figsize"] = 16, 9
 plt.rcParams["figure.dpi"] = 70
 
-from subroutines import *
-
 ###########################################################################
+# Initialize objects
+p = initvals()
+h = heater(p)
 
 # Fit (thermal) circuit parameters
 print("Best fit circuit parameters:")
-lsq = least_squares(fit_params, params0, bounds=(1.e-5, 1.e5), args=(tvec, Tdat-Tdat[ton_idx]), max_nfev=1e5)
+lsq = least_squares(h.fit_params, p.params0, bounds=(1.e-5, 1.e5), args=(p.tvec, p.Tdat-p.Tdat[p.ton_idx]), max_nfev=1e5)
 
 bestfit = dict(zip(["Ch", "Rh", "Ct", "Rt"], 1./lsq.x[:4]))
 bestfit["Tb"] = lsq.x[4]
@@ -26,75 +30,74 @@ for k in ["Ch", "Rh", "Ct", "Rt", "Tb"]:
     print("{}: {}".format(k, bestfit[k]))
 
 # Sanity check: Plot the temperature based on best fit parameters
-soln_on = solve_ivp(lambda t, T: HeaterIVP(t, T, lsq.x, Q = QMAX), [ton, toff], T0, t_eval=tvec[sli_on])
-soln_off = solve_ivp(lambda t, T: HeaterIVP(t, T, lsq.x, Q = 0), [toff, tvec[-1]], [soln_on.y[0][-1], soln_on.y[1][-1]], t_eval=tvec[sli_off])
+soln_on = solve_ivp(lambda t, T: h.HeaterIVP(t, T, lsq.x, Q = p.QMAX), [p.ton, p.toff], p.T0, t_eval=p.tvec[p.sli_on])
+soln_off = solve_ivp(lambda t, T: h.HeaterIVP(t, T, lsq.x, Q = 0), [p.toff, p.tvec[-1]], [soln_on.y[0][-1], soln_on.y[1][-1]], t_eval=p.tvec[p.sli_off])
 soln_all = np.concatenate((soln_on.y[0], soln_off.y[0][1:]))
 
 plt.figure()
 plt.subplot(3, 1, (1,2))
-plt.axhline(Tdat[ton_idx]+273.15, color='C7', linestyle='--')
-plt.axvline(ton, color='C7', linestyle='--')
-plt.axvline(toff, color='C7', linestyle='--')
-plt.plot(tvec, Tdat+273.15, label='Data')
-plt.plot(tvec[ton_idx:], soln_all+Tdat[ton_idx]+273.15, label='LSq fit')
+plt.axhline(p.Tdat[p.ton_idx]+273.15, color='C7', linestyle='--')
+plt.axvline(p.ton, color='C7', linestyle='--')
+plt.axvline(p.toff, color='C7', linestyle='--')
+plt.plot(p.tvec, p.Tdat+273.15, label='Data')
+plt.plot(p.tvec[p.ton_idx:], soln_all+p.Tdat[p.ton_idx]+273.15, label='LSq fit')
 plt.ylabel('Temperature [K]')
 plt.legend()
 plt.tick_params(right=True, top=True)
 plt.subplot(3, 1, 3)
 plt.axhline(0, color='C7', linestyle='--')
-plt.axvline(ton, color='C7', linestyle='--')
-plt.axvline(toff, color='C7', linestyle='--')
-plt.plot(tvec[ton_idx::50], (soln_all-Tdat[ton_idx:]+Tdat[ton_idx])[::50], 'C8', label=r'LSq fit $-$ Data')
+plt.axvline(p.ton, color='C7', linestyle='--')
+plt.axvline(p.toff, color='C7', linestyle='--')
+plt.plot(p.tvec[p.ton_idx::50], (soln_all-p.Tdat[p.ton_idx:]+p.Tdat[p.ton_idx])[::50], 'C8', label=r'LSq fit $-$ Data')
 plt.xlabel("Time [s]")
 plt.ylabel(r'$\Delta T$ [K]')
 plt.tick_params(right=True, top=True)
 plt.legend()
 
 # Fit PID parameters
-soln = solve_ivp(lambda t, T: HeaterIVP(t, T, lsq.x, params_PID=params_PID0), t_span, T0, t_eval=tpid)
+soln = solve_ivp(lambda t, T: h.HeaterIVP(t, T, lsq.x, params_PID=p.params_PID0), p.t_span, p.T0, t_eval=p.tpid)
 
 print("\nBest fit PID gains:")
-lsqPID = least_squares(fit_paramsPID, params_PID0, bounds=(0., 1.e4), args=(tpid, lsq.x), max_nfev=1e5)
+lsqPID = least_squares(h.fit_paramsPID, p.params_PID0, bounds=(0., 1.e4), args=(p.tpid, lsq.x), max_nfev=1e5)
 
 bestfit.update(zip(["Kp", "Ki", "Kd"], lsqPID.x))
 for k in ["Kp", "Ki", "Kd"]:
     print("{}: {}".format(k, bestfit[k]))
 
 # Sanity check: Plot the temperature for the given set point and PID fit
-solnPIDfit = solve_ivp(lambda t, T: HeaterIVP(t, T, lsq.x, params_PID=lsqPID.x), t_span, T0, t_eval=tpid)
+solnPIDfit = solve_ivp(lambda t, T: h.HeaterIVP(t, T, lsq.x, params_PID=lsqPID.x), p.t_span, p.T0, t_eval=p.tpid)
 
 # Write best fit values to disk
-FILEOUT = "bestfit_" + initvals['t_dataset'] + ".out"
-outputdict = OrderedDict(zip(["fileset", "t_dataset", "h_dataset", "time_start", "time_end", "samp_rate"], [FILESET, TDATASET, HDATASET, TSTR, TEND, TSTEP]))
+FILEOUT = "bestfit_" + p.params['t_dataset'] + ".out"
+outputdict = OrderedDict(zip(["fileset", "t_dataset", "h_dataset", "time_start", "time_end", "samp_rate"], [p.FILESET, p.TDATASET, p.HDATASET, p.TSTR, p.TEND, p.TSTEP]))
 outputdict.update(OrderedDict((k, bestfit[k]) for k in ["Ch", "Rh", "Ct", "Rt", "Tb"]))
-outputdict.update(zip(["T0", "setpoint"], [float(Tdat[ton_idx]), SETPOINT]))
+outputdict.update(zip(["T0", "setpoint"], [float(p.Tdat[p.ton_idx]), p.SETPOINT]))
 outputdict.update(OrderedDict((k, bestfit[k]) for k in ["Kp", "Ki", "Kd"]))
 with open(FILEOUT, "w") as outfile:
     json.dump(outputdict, outfile, indent=4)
 
 plt.figure()
 plt.subplot(3, 1, (1, 2))
-plt.axhline(SETPOINT, color='C7', linestyle='--')
-plt.plot(tpid, soln.y[0], 'C1--', label='Initial guess')
-plt.plot(tpid, solnPIDfit.y[0], 'C0', label='Optimized PID')
+plt.axhline(p.SETPOINT, color='C7', linestyle='--')
+plt.plot(p.tpid, soln.y[0], 'C1--', label='Initial guess')
+plt.plot(p.tpid, solnPIDfit.y[0], 'C0', label='Optimized PID')
 plt.ylabel('Temperature [K]')
 plt.legend()
 plt.tick_params(right=True, top=True)
 plt.subplot(3, 1, 3)
-plt.semilogy(tpid, np.abs(SETPOINT - soln.y[0])/SETPOINT, 'C8--')
-plt.semilogy(tpid, np.abs(SETPOINT - solnPIDfit.y[0])/SETPOINT, 'C8')
+plt.semilogy(p.tpid, np.abs(p.SETPOINT - soln.y[0])/p.SETPOINT, 'C8--')
+plt.semilogy(p.tpid, np.abs(p.SETPOINT - solnPIDfit.y[0])/p.SETPOINT, 'C8')
 plt.xlabel("Time [s]")
 plt.ylabel(r'$\Delta T/T_0$')
 plt.tick_params(right=True, top=True)
 
 # Plot the heater power Q(t)
-tt, Tt, Qt, It, tloops = returnAuxArrays()
-newtt = np.delete(tt, list(OrderedDict.fromkeys(tloops)))
-newQt = np.delete(Qt, list(OrderedDict.fromkeys(tloops)))
+newtt = np.delete(h.tt, list(OrderedDict.fromkeys(h.tloops)))
+newQt = np.delete(h.Qt, list(OrderedDict.fromkeys(h.tloops)))
 np.savetxt("qt.out", np.array([newtt, newQt]).T)
 
 plt.figure()
-plt.plot(tt, Qt, 'x--')
+plt.plot(h.tt, h.Qt, 'x--')
 plt.plot(newtt, newQt)
 plt.xlabel("Time [s]")
 plt.ylabel("Power [%]")
--- subroutines.py
+++ subroutines.py
@@ -1,171 +1,172 @@
+# -*- coding: utf-8 -*-
+
 import os, sys, json
 import pygetdata as gd
 import numpy as np
 from scipy import integrate
 from scipy.integrate import solve_ivp
 
-# Load parameters from file, or exit if no file found
-try:
-    with open('params.json') as handle:
-        initvals = json.loads(handle.read())
-except:
-    print("File 'params.json' not found.")
-    sys.exit()
+class initvals:
+    def __init__(self):
+        # Load parameters from file, or exit if no file found
+        try:
+            with open('params.json') as handle:
+                self.params = json.loads(handle.read())
+        except:
+            print("File 'params.json' not found.")
+            sys.exit()
 
-# Constants
-TSTR = int(initvals['time_start'])
-TEND = int(initvals['time_end'])
-TLEN = TEND - TSTR
-TSTEP = float(initvals['samp_rate'])    # timestep [seconds]
+        # Constants
+        self.TSTR = int(self.params['time_start'])
+        self.TEND = int(self.params['time_end'])
+        self.TLEN = self.TEND - self.TSTR
+        self.TSTEP = float(self.params['samp_rate'])    # timestep [seconds]
 
-QMIN, QMAX = 0, 100
+        self.QMIN, self.QMAX = 0, 100
 
-# Physical constants
-circ_params0 = np.array([initvals['Ch0'], initvals['Rh0'], initvals['Ct0'], initvals['Rt0']], dtype=float)
-Tb0 = float(initvals['Tb0'])
-params0 = np.append(1./circ_params0, Tb0)
+        # Physical constants
+        circ_params0 = np.array([self.params['Ch0'], self.params['Rh0'], self.params['Ct0'], self.params['Rt0']], dtype=float)
+        Tb0 = float(self.params['Tb0'])
+        self.params0 = np.append(1./circ_params0, Tb0)
 
-# ODE initial conditions ([T0, Tdot0])
-T0 = [0, 0]
+        # ODE initial conditions ([T0, Tdot0])
+        self.T0 = [0, 0]
 
-# PID parameters
-SETPOINT = float(initvals['setpoint'])
-params_PID0 = np.array([initvals['Kp0'], initvals['Ki0'], initvals['Kd0']], dtype=float)
+        # PID parameters
+        self.SETPOINT = float(self.params['setpoint'])
+        self.params_PID0 = np.array([self.params['Kp0'], self.params['Ki0'], self.params['Kd0']], dtype=float)
 
-# Initialize auxiliary arrays
-tt = np.array([])
-Tt = np.array([])
-Qt = np.array([])
-It = np.array([])
-tloops = []
+        # Load data
+        self.ROOTDIR = self.params['rootdir']
+        self.FILESET = self.params['fileset']
+        self.TDATASET = 't_' + self.params['t_dataset']
+        self.HDATASET = 'h_' + self.params['t_dataset']
+        if ('h_dataset' in self.params.keys()) and (self.params['h_dataset'] != ""):
+            self.HDATASET = 'h_' + self.params['h_dataset']
 
-###########################################################################
-# Load data
-ROOTDIR = initvals['rootdir']
-FILESET = initvals['fileset']
-TDATASET = 't_' + initvals['t_dataset']
-HDATASET = 'h_' + initvals['t_dataset']
-if ('h_dataset' in initvals.keys()) and (initvals['h_dataset'] != ""):
-    HDATASET = 'h_' + initvals['h_dataset']
+        df = gd.dirfile(os.path.join(self.ROOTDIR, self.FILESET))
+        self.Tdat = df.getdata(self.TDATASET)[self.TSTR:self.TEND]
+        self.Qdat = np.maximum(self.QMIN, np.minimum(self.QMAX, df.getdata(self.HDATASET)))[self.TSTR:self.TEND]
 
-df = gd.dirfile(os.path.join(ROOTDIR, FILESET))
-Tdat = df.getdata(TDATASET)[TSTR:TEND]
-Qdat = np.maximum(QMIN, np.minimum(QMAX, df.getdata(HDATASET)))[TSTR:TEND]
+        self.tvec = self.TSTEP * np.arange(self.TLEN)
 
-tvec = TSTEP * np.arange(TLEN)
+        idx_power_on = np.where(self.Qdat>0.5)[0]
+        self.ton_idx = idx_power_on[0]
+        self.toff_idx = idx_power_on[-1] + 1
+        self.sli_on = slice(self.ton_idx, self.toff_idx+1) # +1 because initial conds. needed at endpoints
+        self.sli_off = slice(self.toff_idx, len(self.tvec))
 
-idx_power_on = np.where(Qdat>0.5)[0]
-ton_idx = idx_power_on[0]
-toff_idx = idx_power_on[-1] + 1
-sli_on = slice(ton_idx, toff_idx+1) # +1 because initial conds. needed at endpoints
-sli_off = slice(toff_idx, len(tvec))
+        self.ton = self.ton_idx * self.TSTEP
+        self.toff = self.toff_idx * self.TSTEP
 
-ton = ton_idx * TSTEP
-toff = toff_idx * TSTEP
-
-tpid = np.arange(0, initvals['sim_len'], TSTEP)
-t_span = [tpid[0], tpid[-1]]
+        self.tpid = np.arange(0, self.params['sim_len'], self.TSTEP)
+        self.t_span = [self.tpid[0], self.tpid[-1]]
 
 ###########################################################################
+class heater:
+    def __init__(self, params):
+        # Set parameters
+        self.p = params
 
-def Qfunc(t, T_present, T_deriv, params_PID):
-    Kp, Ki, Kd = params_PID[0], params_PID[1], params_PID[2]
-    try:
-        h = t - tt[-1]
-    except:
-        h = 0.
-    a = 5.e-6 * (100*h)
-    b = 1.0 - a
+        # Initialize auxiliary arrays
+        self.tt = np.array([])
+        self.Tt = np.array([])
+        self.Qt = np.array([])
+        self.It = np.array([])
+        self.tloops = []
 
-    # Proportional term
-    err = SETPOINT - T_present
+    def Qfunc(self, t, T_present, T_deriv, params_PID):
+        Kp, Ki, Kd = params_PID[0], params_PID[1], params_PID[2]
+        try:
+            h = t - self.tt[-1]
+        except:
+            h = 0.
+        a = 5.e-6 * (100*h)
+        b = 1.0 - a
 
-    # Integral term
-    global It
-    integral = a * err
-    try:
-        integral = integral + b*It[-1]
-    except:
-        pass
-    integral = max(0, min(0.5, integral))
-    It = np.append(It, integral)
+        # Proportional term
+        err = self.p.SETPOINT - T_present
 
-    # Derivative term
-    if h != 0:
-        deriv = (Tt[-1] - T_present) / (100*h)
-    else:
-        deriv = -T_deriv
+        # Integral term
+        integral = a * err
+        try:
+            integral = integral + b*self.It[-1]
+        except:
+            pass
+        integral = max(0, min(0.5, integral))
+        self.It = np.append(self.It, integral)
 
-    Q = Kp*err + Ki*integral + Kd*deriv
-    return max(QMIN, min(QMAX, Q))
+        # Derivative term
+        if h != 0:
+            deriv = (self.Tt[-1] - T_present) / (100*h)
+        else:
+            deriv = -T_deriv
 
-def rewindTime(t):
-    global tt, Tt, Qt, It
-    tloops.append(len(tt)-3)
-    tloops.append(len(tt)-2)
-    Tt = Tt[tt<t]
-    Qt = Qt[tt<t]
-    It = It[tt<t]
-    tt = tt[tt<t]
+        Q = Kp*err + Ki*integral + Kd*deriv
+        return max(self.p.QMIN, min(self.p.QMAX, Q))
 
-def writeAuxArrays(t, T, Q):
-    global tt, Tt, Qt
-    Tt = np.append(Tt, T)
-    Qt = np.append(Qt, Q)
-    tt = np.append(tt, t)
+    def rewindTime(self, t):
+        self.tloops.append(len(self.tt)-3)
+        self.tloops.append(len(self.tt)-2)
+        self.Tt = self.Tt[self.tt<t]
+        self.Qt = self.Qt[self.tt<t]
+        self.It = self.It[self.tt<t]
+        self.tt = self.tt[self.tt<t]
 
-def returnAuxArrays():
-    return tt, Tt, Qt, It, tloops
+    def writeAuxArrays(self, t, T, Q):
+        self.Tt = np.append(self.Tt, T)
+        self.Qt = np.append(self.Qt, Q)
+        self.tt = np.append(self.tt, t)
 
-def HeaterIVP(t, T, params_circ, params_PID = None, Q = 0):
-    a, b, c, d = params_circ[0], params_circ[1], params_circ[2], params_circ[3]
-    x1, x2 = T[0], T[1]
-    if params_PID is not None:
-        if len(tt) and t <= tt[-1]:
-            rewindTime(t)
-        Q = Qfunc(t, x1, x2, params_PID)
-        writeAuxArrays(t, x1, Q)
+    def HeaterIVP(self, t, T, params_circ, params_PID = None, Q = 0):
+        a, b, c, d = params_circ[0], params_circ[1], params_circ[2], params_circ[3]
+        x1, x2 = T[0], T[1]
+        if params_PID is not None:
+            if len(self.tt) and t <= self.tt[-1]:
+                self.rewindTime(t)
+            Q = self.Qfunc(t, x1, x2, params_PID)
+            self.writeAuxArrays(t, x1, Q)
 
-    dx1 = x2
-    dx2 = a*b*c*(Q - d*(x1-params_circ[4])) - (a*b + b*c + c*d)*x2
+        dx1 = x2
+        dx2 = a*b*c*(Q - d*(x1-params_circ[4])) - (a*b + b*c + c*d)*x2
 
-    return [dx1, dx2]
+        return [dx1, dx2]
 
-def fit_params(params, tvec, Tt):
-    soln_on = solve_ivp(lambda t, T: HeaterIVP(t, T, params, Q = QMAX), [ton, toff], T0, t_eval=tvec[sli_on])
+    def fit_params(self, params, tvec, Tt):
+        soln_on = solve_ivp(lambda t, T: self.HeaterIVP(t, T, params, Q = self.p.QMAX), [self.p.ton, self.p.toff], self.p.T0, t_eval=tvec[self.p.sli_on])
 
-    T1 = [soln_on.y[0][-1], soln_on.y[1][-1]]
-    soln_off = solve_ivp(lambda t, T: HeaterIVP(t, T, params, Q = 0), [toff, tvec[-1]], T1, t_eval=tvec[sli_off])
+        T1 = [soln_on.y[0][-1], soln_on.y[1][-1]]
+        soln_off = solve_ivp(lambda t, T: self.HeaterIVP(t, T, params, Q = 0), [self.p.toff, tvec[-1]], T1, t_eval=tvec[self.p.sli_off])
 
-    soln_all = np.concatenate((soln_on.y[0], soln_off.y[0][1:]))
-    return (Tt[ton_idx:] - soln_all)
+        soln_all = np.concatenate((soln_on.y[0], soln_off.y[0][1:]))
+        return (Tt[self.p.ton_idx:] - soln_all)
 
-def fit_paramsPID(params, tvec, iparams_circ):
-    solnPID = solve_ivp(lambda t, T: HeaterIVP(t, T, iparams_circ, params_PID=params), t_span, T0, t_eval=tvec)
+    def fit_paramsPID(self, params, tvec, iparams_circ):
+        solnPID = solve_ivp(lambda t, T: self.HeaterIVP(t, T, iparams_circ, params_PID=params), self.p.t_span, self.p.T0, t_eval=tvec)
 
-    # Objective function to minimize
-    weights = np.array([initvals['weight_ovrsh'], initvals['weight_trise'], initvals['weight_tset'], initvals['weight_errss']])
-    extrema_list = np.where(np.diff(np.sign(solnPID.y[1])))[0]  # returns indices just before a sign change in derivative
-    if solnPID.y[1][0] == 0:
-        extrema_list = extrema_list[1:]     # if initial conditions have deriv = 0, ignore initial point
+        # Objective function to minimize
+        weights = np.array([self.p.params['weight_ovrsh'], self.p.params['weight_trise'], self.p.params['weight_tset'], self.p.params['weight_errss']])
+        extrema_list = np.where(np.diff(np.sign(solnPID.y[1])))[0]  # returns indices just before a sign change in derivative
+        if solnPID.y[1][0] == 0:
+            extrema_list = extrema_list[1:]     # if initial conditions have deriv = 0, ignore initial point
 
-    ovrsh, t_rise, t_set, err_ss = 1.e8, 1.e8, 1.e8, 1.e8
+        ovrsh, t_rise, t_set, err_ss = 1.e8, 1.e8, 1.e8, 1.e8
 
-    # Overshoot
-    if len(extrema_list) > 0:
-        first_peak = extrema_list[0]
-        ovrsh = max(solnPID.y[0][first_peak], solnPID.y[0][first_peak+1]) - SETPOINT
+        # Overshoot
+        if len(extrema_list) > 0:
+            first_peak = extrema_list[0]
+            ovrsh = max(solnPID.y[0][first_peak], solnPID.y[0][first_peak+1]) - self.p.SETPOINT
 
-    # Rise time
-    t_rise = 0.
+        # Rise time
+        t_rise = 0.
 
-    # Settling time
-    T_TOL = initvals['T_tol']
-    t_setflr = np.where(np.abs(np.flip(solnPID.y[0]) - SETPOINT) > T_TOL)[0][0]
-    t_set = initvals['sim_len'] - t_setflr
+        # Settling time
+        T_TOL = self.p.params['T_tol']
+        t_setflr = np.where(np.abs(np.flip(solnPID.y[0]) - self.p.SETPOINT) > T_TOL)[0][0]
+        t_set = self.p.params['sim_len'] - t_setflr
 
-    # Steady state error
-    if t_set < initvals['sim_len']:
-        err_ss = np.mean(SETPOINT - solnPID.y[0][t_set:])
+        # Steady state error
+        if t_set < self.p.params['sim_len']:
+            err_ss = np.mean(self.p.SETPOINT - solnPID.y[0][t_set:])
 
-    return np.array([ovrsh, t_rise, t_set, err_ss]) * weights
+        return np.array([ovrsh, t_rise, t_set, err_ss]) * weights
